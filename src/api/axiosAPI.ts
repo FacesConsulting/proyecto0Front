4636c594ec1bbe2bbff743b44a781861fb94e717
Dylan Jay Flores <dylanjay153@gmail.com>
2023-05-09 10:28:13 -0600

    diseño de verificacion de correo

--- src/api/axiosAPI.ts
+++ src/api/axiosAPI.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 //* Develop */
 export const api = axios.create({
-  baseURL: 'http://127.0.0.1:8081/',
+  baseURL: 'http://192.168.0.14:8081/',
   headers: { 'Content-Type': 'application/json' }
 })
 
