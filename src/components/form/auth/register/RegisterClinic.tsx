2c00fdba7d5589902ba14bb82597d2351591628c
Dylan Jay Flores <dylanjay153@gmail.com>
2023-05-11 17:11:26 -0600

    Registro clinica

--- src/components/form/auth/register/RegisterClinic.tsx
+++ src/components/form/auth/register/RegisterClinic.tsx
@@ -13,8 +13,7 @@ import {
   InputLabel,
   OutlinedInput,
   TextField,
-  Typography,
-  Button
+  Typography
 } from '@mui/material'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -24,24 +23,11 @@ import { ClinicType } from '@/interfaces/auth/auth.interface'
 import { useFormik } from 'formik'
 
 import React, { useEffect, useState } from 'react'
-const styling = {
-  backgroundImage: `url(${img.src})`
-}
-const logoStyle = {
-  borderRadius: '100%',
-  width: '75%',
-  aspectRatio: '16/16',
-  objectFit: 'cover'
-}
-// // import { Visibility, VisibilityOff } from '@mui/icons-material'
-// import { useFormik } from 'formik'
-// import { SingUpInterface } from '@/interfaces/auth/auth.interface'
-// import { validationSchemaSignUp } from '@/validations/Login/ValidationLogin'
-// import { fetchingDataEncrypted } from '@/utils/utils'
-// import Swal from 'sweetalert2'
-// import { useRouter } from 'next/navigation'
+import { Toaster, toast } from 'react-hot-toast'
+import ToastCustom from '@/components/toast/ToastCustom'
 
 const RegisterClinic = () => {
+  const [file, setFile] = useState<File | null>(null)
   const initialValues: ClinicType = {
     razonSocial: '',
     rfc: '',
@@ -61,28 +47,65 @@ const RegisterClinic = () => {
       }
     })
 
-  const onImageChange = (e: Event) => {
-    // formik.values.logoSource = e.target
-    values.logoSource = String(e.target.files[0].name)
-    console.log(e.target.files[0])
-    console.log('Imagen seleccionada')
+  const handleChangeFile = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputElement = event.target as HTMLInputElement
+    if (
+      inputElement &&
+      inputElement.files &&
+      inputElement.files.length > 0 &&
+      inputElement.files[0].size <= 500000
+    ) {
+      const file = inputElement.files[0]
+      setFile(file)
+      return
+    }
+
+    toast.custom((t) => <ToastCustom t={t} />)
   }
 
   return (
     <>
-      <div
-        className='lg:w-1/2 hidden lg:flex items-center bg-login border'
-        style={styling}>
-        <Button component='label'>
-          <input
-            hidden
-            accept='image/*'
-            multiple
-            type='file'
-            onChange={onImageChange}
-          />
-          <Image src={values.logoSource} alt={Public} style={logoStyle} />
-        </Button>
+      <div className='lg:w-1/2 lg:flex flex-col items-center justify-center w-full p-4'>
+        <Toaster position='top-left' />
+        <div>
+          <div className='mx-auto mb-4 flex justify-center items-center text-gray-400 border border-gray-200 border-dashed rounded-full cursor-pointer w-52 h-52'>
+            <div className='flex relative flex-col gap-2 justify-center items-center text-gray-400 bg-gray-100 border border-gray-200 border-dashed rounded-full w-48 h-48 overflow-hidden'>
+              <input
+                type='file'
+                name='archivo'
+                id='archivo'
+                accept='.jpge,.jpg'
+                onChange={handleChangeFile}
+                className='absolute inset-0 z-50 w-full h-full p-0 m-0 outline-none opacity-0 cursor-pointer'
+              />
+              {file !== null
+                ? (
+                <Image
+                  className='absolute inset-0 z-0 object-cover w-full h-full border-4 border-white'
+                  src={URL.createObjectURL(file || new Blob())}
+                  alt={file.name}
+                  fill
+                />
+                  )
+                : (
+                <>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='30'
+                    height='30'
+                    fill={'#9ca3af'}
+                    viewBox='0 0 24 24'>
+                    <path d='M5 4h-3v-1h3v1zm8 6c-1.654 0-3 1.346-3 3s1.346 3 3 3 3-1.346 3-3-1.346-3-3-3zm11-5v17h-24v-17h5.93c.669 0 1.293-.334 1.664-.891l1.406-2.109h8l1.406 2.109c.371.557.995.891 1.664.891h3.93zm-19 4c0-.552-.447-1-1-1s-1 .448-1 1 .447 1 1 1 1-.448 1-1zm13 4c0-2.761-2.239-5-5-5s-5 2.239-5 5 2.239 5 5 5 5-2.239 5-5z' />
+                  </svg>
+                  <p>Subir logo</p>
+                </>
+                  )}
+            </div>
+          </div>
+          <small className='mt-4 text-gray-500'>
+            Permitidos *.jpeg, *.jpg tamaño máximo de 3,1 MB
+          </small>
+        </div>
       </div>
       <div className='w-full lg:w-1/2 flex justify-center items-start lg:items-center p-4'>
         <Box className='w-full px-4'>
