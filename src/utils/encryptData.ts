e885da54243465d6ac598e03941d563ad46c4d41
jguzmancr <jguzmancr@aeromexico.com>
2023-05-19 16:01:43 -0600

    Send Doctor Data

--- src/utils/encryptData.ts
+++ src/utils/encryptData.ts
@@ -1,5 +1,6 @@
 import { api } from '@/api/axiosAPI'
 import { ClinicType } from '@/interfaces/auth/auth.interface'
+import { DoctorType } from '@/interfaces/clinic/doctor'
 import { AxiosResponse } from 'axios'
 import * as crypto from 'crypto'
 import { JWTPayload, JWTVerifyOptions, jwtVerify } from 'jose'
@@ -73,6 +74,29 @@ export const preparedFormDataClinic = (values: ClinicType) => {
   formData.append('iv', iv.toString('base64'))
   if (values.logoSource !== null) {
     formData.append('logo', values.logoSource)
+  }
+  return formData
+}
+
+export const preparedFormDataDoctor = (values: DoctorType) => {
+  const key = crypto.randomBytes(16)
+  const iv = crypto.randomBytes(16)
+
+  // Crear instancia de cifrado
+  const cipher = crypto.createCipheriv('aes-128-cbc', key, iv)
+
+  // Cifrar los datos
+  let encryptedData = cipher.update(JSON.stringify(values), 'utf-8', 'base64')
+  encryptedData += cipher.final('base64')
+
+  const formData = new FormData()
+  formData.append('data', encryptedData)
+  formData.append('key', key.toString('base64'))
+  formData.append('iv', iv.toString('base64'))
+  if (values.especialidad !== null) {
+    for (const element of values.especialidad) {
+      formData.append(('especialidad '), element.especialidad)
+    }
   }
   return formData
 }
